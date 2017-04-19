20816c385f473c47cfa5ceb22d710495f3951f3c
YOUJUN LEE <injxyj@gmail.com>
2017-04-20 01:36:40 +0900

    메인페이지 초안 완성 및 my page page 추가

--- itaxi_app/src/app/app.module.ts
+++ itaxi_app/src/app/app.module.ts
@@ -3,38 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MyPage } from '../pages/my-page/my-page';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage, TabsPage, MyPage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  entryComponents: [ MyApp, HomePage, TabsPage, MyPage],
+  providers: [ StatusBar, SplashScreen, {provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {}
---
+++ itaxi_app/src/pages/my-page/my-page.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
+import { MyPage } from './my-page';
+
+@NgModule({
+  declarations: [
+    MyPage,
+  ],
+  imports: [
+    
+  ],
+  exports: [
+    MyPage
+  ]
+})
+export class MyPageModule {}
