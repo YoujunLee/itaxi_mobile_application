a122fd5b78663877c6ad3f150ad88d52345801af
YOUJUN LEE <injxyj@gmail.com>
2017-04-20 02:33:12 +0900

    makeRoom segment 설정 및 초안 시

--- itaxi_app/src/pages/make-room/make-room.ts
+++ itaxi_app/src/pages/make-room/make-room.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the MakeRoom page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-make-room',
@@ -20,5 +14,7 @@ export class MakeRoom {
   ionViewDidLoad() {
     console.log('ionViewDidLoad MakeRoom');
   }
+
+  type: string="taxi";
 
 }
