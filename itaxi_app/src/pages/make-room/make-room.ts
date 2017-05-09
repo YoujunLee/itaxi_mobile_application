00676486e3c36e633f1ddd68faa21bffafc435c4
YOUJUN LEE <injxyj@gmail.com>
2017-05-09 11:56:40 +0900

    방 생성 1차 완성 #todo 방 참여하기

--- itaxi_app/src/pages/make-room/make-room.ts
+++ itaxi_app/src/pages/make-room/make-room.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DatePicker } from 'ionic2-date-picker/ionic2-date-picker';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @IonicPage()
 @Component({
@@ -9,24 +11,40 @@ import { DatePicker } from 'ionic2-date-picker/ionic2-date-picker';
   providers: [ DatePicker ]
 })
 export class MakeRoom {
-today;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public datePicker: DatePicker) {
-     this.datePicker.onDateSelected.subscribe( 
-      (date) => {
-        console.log(date);
-    });
 
-     this.today = new Date().toISOString();
+date: any;
+time: any;
+start: string;
+arrive: string;
+population: number;
+type: string="taxi";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public datePicker: DatePicker) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MakeRoom');
   }
 
-  type: string="taxi";
+  submit(){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
 
+    let body = {
+      start: this.start,
+      arrive: this.arrive,
+      date: this.date,
+      time: this.time,
+      population: this.population
+    };
 
-  showCalendar(){
-    this.datePicker.showCalendar();
-  }
+    let data=JSON.stringify(body);
+    console.log(body);
+    this.http.post('/api/make_room.php', data,headers)
+    .map(res=>res.json())
+      .subscribe(res=>{
+        console.log(JSON.stringify(body));
+        console.log(res);
+      });
+  } 
 }
