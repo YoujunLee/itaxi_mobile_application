36815a7e2dfa4766a9982177d08a42b3d5c027a3
YOUJUN LEE <injxyj@gmail.com>
2017-05-09 22:02:55 +0900

    참가인원수 출력 완

--- itaxi_app/src/pages/home/home.ts
+++ itaxi_app/src/pages/home/home.ts
@@ -12,12 +12,16 @@ import { InnerRoom } from '../inner-room/inner-room';
 })
 export class HomePage {
  items: any;
- loading: any;
+ items_count: any;
  post_id: any;
+ post_arrs: any;
+ post_counts: any;
+ i:number;
+
  stu_id= 21101002;
  name =  "이유준";
  cellphone	= "010-4408-4262";
-
+ 
  constructor(public navCtrl: NavController, public http : Http, public loadingCtrl:LoadingController, public httpProvider:GetRoom) {
     this.getdata(); 
   }
@@ -25,6 +29,15 @@ export class HomePage {
   this.httpProvider.getJsonData().subscribe(
     result => {
       this.items=result;
+      this.post_arrs=Array(result.length);
+      this.post_counts=Array(result.length);
+
+      for(this.i=0; this.i<result.length; this.i++){
+        this.post_arrs[this.i]=this.items[this.i].post_id;
+        this.getUser(this.i);
+      }
+      console.log(this.post_arrs);
+      console.log(this.post_counts);
     },
     err =>{
       console.error("Error : "+err);
@@ -34,6 +47,24 @@ export class HomePage {
     }
   );
 }
+  
+  getUser(j){
+     /*참가자 숫자 출력 시작*/
+             this.http.get('http://itaxi.handong.edu/api/get_room_info.php'+'?'+this.post_arrs[j]).map(res => res.json()).subscribe(
+                result => {
+                   this.items_count=result;
+                   this.post_counts[j]=this.items_count[1].count;
+                  //console.log(this.items_count[1].count);
+                },
+                err =>{
+                  console.error("Error : "+err);
+                } ,
+                () => {
+                }
+              );
+        /*--------------------------*/
+  }
+
 
   room(){
     this.navCtrl.push(InnerRoom,{post_id:this.post_id});
